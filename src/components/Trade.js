0688dfe1b4be5a92db15ed5629c0295670f7325a
3nergetik <75131588+3nergetik@users.noreply.github.com>
2022-05-05 21:14:10 +0500

    Update and rename Exchange.js to Trade.js

---
+++ src/components/Trade.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+/**
+ * Компонент, показывающий изменение основных биржевых инструментов
+ */
+
+export default function Trade({currency}) {
+    return 'Биржевой компонент';
+}
